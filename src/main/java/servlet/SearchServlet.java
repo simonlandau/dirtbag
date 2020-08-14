0bc6aff06ca1788c10c788d737a55a4149b64f24
landa <simonlan@usc.edu>
2020-08-14 17:55:40 -0400

    adding search functionality

--- src/main/java/servlet/SearchServlet.java
+++ src/main/java/servlet/SearchServlet.java
@@ -1,6 +1,11 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -20,8 +25,36 @@ public class SearchServlet extends HttpServlet {
             throws ServletException, IOException {
     	
     		System.out.println("Enter Search Servlet");
-        	String query = req.getParameter("query");
-        	System.out.println(query);
+        	String query =  "%" + req.getParameter("query") + "%";
+        	System.out.println("The query is " + query);
+        	
+        	try {
+    			
+    			Class.forName("com.mysql.cj.jdbc.Driver");
+    			
+    			Connection conn = null;
+    			ResultSet rs = null;
+    			PreparedStatement ps = null;
+    			
+    			System.out.println("Trying to connect to the database...");
+    			conn = DriverManager.getConnection("jdbc:mysql://us-cdbr-east-02.cleardb.com:3306/heroku_2ee69ac199fd2d5?user=b002e739943731&password=7098f952&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC");
+    			System.out.println("Succesfully connected to the database");
+    			
+    			System.out.println("running MySQL statement...");
+    			ps = conn.prepareStatement("SELECT * FROM deftable WHERE Word LIKE ?");
+    			ps.setString(1,query);
+    			rs = ps.executeQuery();
+    			System.out.println("MySQL statement ran succesfully");
+    			
+    			System.out.println("Print results to the console");
+    			while(rs.next()) {
+    				System.out.println( rs.getString("Word") + ": " + rs.getString("Definitions"));
+    			}
+    			
+        	}catch(Exception e) {
+        		System.out.println("ur shit is fucked");
+        		System.out.println(e.getMessage());
+        	}
     }
     
 }
